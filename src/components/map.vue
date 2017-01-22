7b66d2e4f63f395014c634d988ab98891a176bb0
zzes <kuanglong0312@sina.com>
2017-01-22 09:16:45 +0800

    基地所在页面

---
+++ src/components/map.vue
@@ -0,0 +1,28 @@
+
+<template>
+<center>
+<div id="map-container"></div></center>
+</template>
+ 
+<style>
+#map-container {
+    height: 600px;
+    width:80%;
+}
+</style>
+ 
+
+<script type="text/javascript">
+import AMap from 'AMap'
+
+export default{
+ ready () {
+    const map = new AMap.Map('map-container', {
+      zoom: 15,
+      center: [117.84032,30.964879]
+    })
+   
+    console.log(map)
+  }	
+}
+</script> 
