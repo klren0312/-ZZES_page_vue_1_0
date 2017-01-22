c916243dcbbffed37a905d62e1047715947b4f84
zzes <kuanglong0312@sina.com>
2017-01-22 16:01:19 +0800

    产品页面

---
+++ src/components/subject.vue
@@ -0,0 +1,78 @@
+<template>
+	<center><h1>治电科技作品展</h1></center>
+    <Row>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    	        <div style="text-align:center">
+    	            <img src="../images/subjects/8.jpg" style="width:100%">
+    	            <h3>治电科技2017鸡年吉祥物</h3>
+    	        </div>
+    	    </Card>
+    	</i-col>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/1.jpg" alt="" style="width:100%">
+    				<h3>治电科技陆龟生态箱</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/2.jpg" alt="" style="width:100%">
+    				<h3>治电科技智能登山杖</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    </Row>
+    <br>
+    <Row>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/3.jpg" alt="" style="width:100%">
+    				<h3>治电科技智能手环</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/4.jpg" alt="" style="width:100%">
+    				<h3>行星齿轮无级变速器</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    	<i-col span="8">
+    		<Card style="width:400px">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/6.jpg" alt="" style="width:100%">
+    				<h3>治电科技智能传送带</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    </Row>
+    <Row>
+    	<i-col span="12">
+    		<Card style="width:100%">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/5.jpg" alt="" style="width:100%">
+    				<h3>治电科技智慧宠物屋</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    	<i-col span="12">
+    		<Card style="width:100%">
+    			<div style="text-align:center">
+    				<img src="../images/subjects/7.jpg" alt="" style="width:100%">
+    				<h3>治电科技新概念笔记本</h3>
+    			</div>
+    		</Card>
+    	</i-col>
+    </Row>
+
+</template>
+<script>
+export default{}      
+</script>
