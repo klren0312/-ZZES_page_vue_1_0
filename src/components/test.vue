33012ca522b412728b082a2aee132bd9e32a70e8
zzes <kuanglong0312@sina.com>
2017-01-22 09:18:44 +0800

    首页

---
+++ src/components/test.vue
@@ -0,0 +1,51 @@
+<template>
+	<Row>
+		<i-col span="24"><center>
+			<img src="../images/zzes2017.png" alt="zzes2017" style="width:80%;height:500px;"></center>
+		</i-col>
+	</Row>
+	<center>
+	<Row>
+		<i-col span="8">
+			<Card style="width:400px">
+			    <div style="text-align:center">
+			        <img src="../images/1.jpg" style="width:50%">
+			        <h3>治中电科，求真，务实，诚信，合作；<br>只为追求高端科技</h3>
+			    </div>
+			</Card>
+		</i-col>	        
+		<i-col span="8">
+			<Card style="width:400px">
+			    <div style="text-align:center">
+			        <img src="../images/2.png" style="width:50%">
+			        <h3>治电科技，治中电科与望创CCW以及WSMR团队联合创建，汇聚各大精英</h3>
+			    </div>
+			</Card>
+		</i-col>
+		<i-col span="8">
+			<Card style="width:400px">
+			    <div style="text-align:center">
+			        <img src="../images/3.jpg" style="width:50%">
+			        <h3>产品范围，涉及方方面面，忠于天神，一切技术只为天神，不对外公开</h3>
+			    </div>
+			</Card>
+		</i-col>		
+	</Row>
+</center>
+	<hr>
+	<hr>
+	<center>
+		<a href="http://vuejs.org.cn/">Vuejs官方文档&nbsp;&nbsp;</a>
+		<a href="http://router.vuejs.org/zh-cn/index.html">Vue-router&nbsp;&nbsp;</a>
+		<a href="http:/zzesxiao.leanote.com">治电小白菜博客&nbsp;&nbsp;</a>	
+		<br>
+		Copyright © 2009-2017 治电科技
+	</center>
+</template>
+ 
+
+ 
+
+<script type="text/javascript">
+	export default{}
+</script> 
