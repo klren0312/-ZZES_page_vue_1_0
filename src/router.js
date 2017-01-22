4cb1a95f4fc94c3b7c04c9e3a0bfc0f8d676ab7e
zzes <kuanglong0312@sina.com>
2017-01-22 09:26:27 +0800

    路由配置

--- src/router.js
+++ src/router.js
@@ -6,6 +6,26 @@ const routers = {
         component (resolve) {
             require(['./views/index.vue'], resolve);
         }
+    },
+    '/test':{
+    	component(resolve){
+    		require(['./components/test.vue'],resolve);
+    	}
+    },
+    '/map':{
+    	component(resolve){
+    		require(['./components/map.vue'],resolve);
+    	}
+    },
+    '/subject':{
+    	component(resolve){
+    		require(['./components/subject.vue'],resolve);
+    	}
+    },
+    '/aboutus':{
+    	component(resolve){
+    		require(['./components/aboutus.vue'],resolve);
+    	}
     }
 };
 export default routers;
